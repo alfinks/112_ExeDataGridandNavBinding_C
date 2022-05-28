e8370f8e18a27bc19f2a1be4545377cdb3e27138
alfinks <maulanaafiansyah@gmail.com>
2022-05-28 23:28:55 +0700

    Memperbaiki Login

--- ExeDataGridandNavBinding/Form1.cs
+++ ExeDataGridandNavBinding/Form1.cs
@@ -29,8 +29,24 @@ namespace ExeDataGridandNavBinding
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form2 alfin = new Form2();
-            alfin.Show();
+            {
+                // Membuat Code Login dan Pindah Ke halaman Dashboard atau Halaman Selanjutnya 
+                // Code Perulangan Username dan Password dan pembuatan message Box
+
+                if (textBox1.Text.Equals("") || textBox2.Text.Equals(""))
+                {
+                    MessageBox.Show("Data Tidak Boleh Kosong", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (textBox1.Text.Equals("alfin") && textBox2.Text.Equals("123"))
+                {
+                    Form2 alfin = new Form2();
+                    alfin.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Periksa Kembali Username dan Password Anda !!!!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
